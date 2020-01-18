b2cf09892cdde7920856b47bab01202e739f419f
kathleenalexandra <kathleenbryan24@gmail.com>
2020-01-18 14:07:29 -0500

    add

---
+++ Assets/ClassRoomAssets/DebugScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        TestFunction(); 
+    }
+
+
+    void TestFunction() {
+    	Debug.Log("Script Fired Test"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
