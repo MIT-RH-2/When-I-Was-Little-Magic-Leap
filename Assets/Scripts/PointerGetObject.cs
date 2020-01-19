022ada9bbceb8601f97b9f666177ea788ab30e38
kathleenalexandra <kathleenbryan24@gmail.com>
2020-01-19 10:07:36 -0500

    x

--- Assets/Scripts/PointerGetObject.cs
+++ Assets/Scripts/PointerGetObject.cs
@@ -8,5 +8,51 @@ public class PointerGetObject : MonoBehaviour
 {
 
 
+
+    public Text storyText; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MagicLeapTools.Pointer pointer = gameObject.GetComponent<MagicLeapTools.Pointer>(); 
+        if (pointer.Target != null)  {
+            Debug.Log("got game object" + pointer.Target.tag.ToString()); 
+            TagFound(pointer.Target.tag.ToString()); 
+        } else {
+            Debug.Log("nothing found"); 
+        }
+        
+    }
+
+
+    void TagFound(string obTag) {
+        Debug.Log("This is the ob tag!" + obTag); 
+
+		switch (obTag)
+		    {
+		        case "globe":
+		            Debug.Log("Globe");
+		            storyText.text = obTag; 
+		            break;
+		        case "backpack":
+		             storyText.text = obTag; 
+		            break;
+		        default:
+		            Debug.Log ("Other");
+		            break;
+   	 	}
+
+    }
+
+
+
+
+
 }
 
