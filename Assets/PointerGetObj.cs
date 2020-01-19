ce61bc84b67e5d16343651aabddf9dc2f608d887
kathleenalexandra <kathleenbryan24@gmail.com>
2020-01-18 21:22:33 -0500

    working pointer

---
+++ Assets/PointerGetObj.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerGetObj : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MagicLeapTools.Pointer pointer = gameObject.GetComponent<MagicLeapTools.Pointer>(); 
+        if (pointer.Target != null)  {
+            Debug.Log("got game object" ); 
+        } else {
+            Debug.Log("NA"); 
+        }
+        
+    }
+}
