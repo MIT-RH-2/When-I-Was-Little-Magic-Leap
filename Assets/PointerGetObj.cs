652c7c0a436cc3ade9b89c7680d4cc0553d280ee
kathleenalexandra <kathleenbryan24@gmail.com>
2020-01-19 08:30:45 -0500

    updating scene

--- Assets/PointerGetObj.cs
+++ Assets/PointerGetObj.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class PointerGetObj : MonoBehaviour
 {
+
+    public Text storyText; 
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +22,8 @@ public class PointerGetObj : MonoBehaviour
         MagicLeapTools.Pointer pointer = gameObject.GetComponent<MagicLeapTools.Pointer>(); 
         if (pointer.Target != null)  {
             Debug.Log("got game object" ); 
-            Debug.Log("got game object" + pointer.Target.tag); 
+            Debug.Log("got game object" + pointer.Target.tag.ToString()); 
+            TagFound(pointer.Target.tag.ToString()); 
         } else {
             Debug.Log("NA"); 
         }
@@ -25,6 +32,25 @@ public class PointerGetObj : MonoBehaviour
 
 
 
+    void TagFound(string obTag) {
+        Debug.Log("This is the ob tag!" + obTag); 
+
+switch (obTag)
+    {
+        case "globe":
+            Debug.Log("One");
+            break;
+        case "backpack":
+            Debug.Log("Two");
+            Debug.Log("Two");
+            break;
+        default:
+            Debug.Log ("Other");
+            break;
+    }
+
+
+    }
 
 
 }
