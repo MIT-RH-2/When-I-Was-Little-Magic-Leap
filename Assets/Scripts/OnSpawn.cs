9e49df5b462f001f1bce66f0189d73f8360915fd
ibrews <ibrews@users.noreply.github.com>
2020-01-19 12:04:40 -0500

    final toward push

--- Assets/Scripts/OnSpawn.cs
+++ Assets/Scripts/OnSpawn.cs
@@ -10,9 +10,9 @@ public class OnSpawn : MonoBehaviour {
     // Start is called before the first frame update
     void Start()
     {
-        if (GameObject.FindGameObjectWithTag("wait").GetComponent<Camera>() != null) _waitCam = GameObject.FindGameObjectWithTag("wait").GetComponent<Camera>();
+        //if (GameObject.FindGameObjectWithTag("wait").GetComponent<Camera>() != null) _waitCam = GameObject.FindGameObjectWithTag("wait").GetComponent<Camera>();
         if (GameObject.FindGameObjectWithTag("env").transform != null) _environment = GameObject.FindGameObjectWithTag("env").transform;
-        if (_waitCam != null) _waitCam.gameObject.SetActive(false);
+        //if (_waitCam != null) _waitCam.gameObject.SetActive(false);
         if (_environment != null) this.gameObject.transform.parent = _environment;
     }
 
